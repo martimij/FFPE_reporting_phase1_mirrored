d6a27c2fbf08fe4d24f74f8e39d68c0bb14b5b98
Martina Mijuskovic <martina.mijuskovic@genomicsengland.co.uk>
2017-12-04 14:23:23 +0000

    corrected VCF merge scripts (v3)

---
+++ Scripts/AFtoSomaticVCF_FF_full_2f_v3.bsub.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#BSUB -J addAF_merged_FF_full_VCF
+#BSUB -n 1
+#BSUB -q bio
+#BSUB -o /home/mmijuskovic/bsub_outputs/output.%J
+#BSUB -e /home/mmijuskovic/bsub_outputs/error.%J
+ 
+# Set the environment
+source /etc/profile.d/modules.sh
+source /lsf/prod/conf/profile.lsf
+
+module load bcftools 
+module load vt/0.577
+
+# Working directory where merged VCF will be written
+workdir="/home/mmijuskovic/small_variant_freq/FF/mergedVCF/final_for_production/"
+
+# List of VCFs to be merged (need to have GT, PL values)
+vcf_list="/home/mmijuskovic/small_variant_freq/FF/mergedVCF/FF_PCRfree_nano_mainProgram_2017.txt"
+
+# Call merging script
+/home/mmijuskovic/small_variant_freq/AFtoSomaticVCF_v3.sh $workdir $vcf_list
